245315cf92fc58dc986d441be5f4caae1a0baf87
ArtemShustov <md.artem.shustov@gmail.com>
2026-07-31 16:00:27 +0500

    feat: split device pairing from LocalPlayer

--- Assets/MySunnyBakery/Scripts/Core/LocalPlayer.cs
+++ Assets/MySunnyBakery/Scripts/Core/LocalPlayer.cs
@@ -1,31 +1,20 @@
-﻿using System.Linq;
-using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.LowLevel;
-using UnityEngine.InputSystem.Users;
+﻿using UnityEngine;
 
 namespace MySunnyBakery.Core {
 	public class LocalPlayer: MonoBehaviour {
 		[SerializeField] private GameCamera _camera;
+		
 		public InputActions Actions { get; private set; }
-		private InputUser _user;
 
-		public void Awake() {
+		private void Awake() {
 			Actions = new InputActions();
 			Actions.Enable();
 			
-			_user = InputUser.CreateUserWithoutPairedDevices();
 			_camera.Initialize(this);
-
-			InputUser.listenForUnpairedDeviceActivity += 1;
-			InputUser.onUnpairedDeviceUsed += OnUnpairedDeviceUser;
 		}
-		public void OnDestroy() {
+		private void OnDestroy() {
 			Actions.Disable();
-			_user.UnpairDevicesAndRemoveUser();
-			
-			InputUser.listenForUnpairedDeviceActivity -= 1;
-			InputUser.onUnpairedDeviceUsed -= OnUnpairedDeviceUser;
+			Actions.Dispose();
 		}
 
 		public void TakeControl(IControllable controllable) {
@@ -34,34 +23,6 @@ namespace MySunnyBakery.Core {
 		}
 		public void ReleaseControl(IControllable controllable) {
 			controllable.ReleaseControl();
-		}
-		
-		public void PairDevice(InputDevice device) {
-			InputUser.PerformPairingWithDevice(device, _user);
-		}
-		public void UnpairDevices() {
-			_user.UnpairDevices();
-		}
-		public bool IsPaired(InputDevice device) {
-			return _user.pairedDevices.Contains(device);
-		}
-		
-		private void OnUnpairedDeviceUser(InputControl control, InputEventPtr @event) {
-			OnDeviceTriggered(control.device);
-		}
-		private void OnDeviceTriggered(InputDevice device) {
-			var isKeyboardOrMouse = device is Keyboard or Mouse;
-
-			var isDeviceUsed = IsPaired(device);
-			if (!isDeviceUsed) {
-				UnpairDevices();
-				if (isKeyboardOrMouse) {
-					PairDevice(Mouse.current);
-					PairDevice(Keyboard.current);
-				} else {
-					PairDevice(device);
-				}
-			}
 		}
 	}
 }
---
+++ Assets/MySunnyBakery/Scripts/Core/PlayerDevicePairing.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.InputSystem.Users;
+
+namespace MySunnyBakery.Core {
+	[RequireComponent(typeof(LocalPlayer))]
+	public class PlayerDevicePairing: MonoBehaviour {
+		public bool AutoPair { get; set; } = true;
+		
+		private LocalPlayer _player;
+		private InputUser _user;
+
+		private void Awake() {
+			_player = GetComponent<LocalPlayer>();
+			
+			_user = InputUser.CreateUserWithoutPairedDevices();
+
+			InputUser.listenForUnpairedDeviceActivity += 1;
+			InputUser.onUnpairedDeviceUsed += OnUnpairedDeviceUser;
+		}
+
+		private void Start() {
+			_user.AssociateActionsWithUser(_player.Actions);
+		}
+
+		private void OnDestroy() {
+			_user.UnpairDevicesAndRemoveUser();
+			
+			InputUser.listenForUnpairedDeviceActivity -= 1;
+			InputUser.onUnpairedDeviceUsed -= OnUnpairedDeviceUser;
+		}
+		
+		public void PairDevice(InputDevice device) {
+			InputUser.PerformPairingWithDevice(device, _user);
+		}
+		public void UnpairDevices() {
+			_user.UnpairDevices();
+		}
+		public bool IsPaired(InputDevice device) {
+			return _user.pairedDevices.Contains(device);
+		}
+		
+		private void OnUnpairedDeviceUser(InputControl control, InputEventPtr @event) {
+			if (!AutoPair) {
+				return;
+			}
+			OnDeviceTriggered(control.device);
+		}
+		private void OnDeviceTriggered(InputDevice device) {
+			var isKeyboardOrMouse = device is Keyboard or Mouse;
+
+			var isDeviceUsed = IsPaired(device);
+			if (!isDeviceUsed) {
+				UnpairDevices();
+				if (isKeyboardOrMouse) {
+					PairDevice(Mouse.current);
+					PairDevice(Keyboard.current);
+				} else {
+					PairDevice(device);
+				}
+			}
+		}
+	}
+}
